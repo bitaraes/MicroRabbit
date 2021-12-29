7cc262792feeaeedd2f75d5ee4e70e4f35a4b3f2
Igor <igor_oliveira02@hotmail.com>
2021-12-29 11:13:21 -0300

    Project complete with MVC app to create banking transfers

---
+++ MicroRabbit.MVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using MicroRabbit.MVC.Models;
+using MicroRabbit.MVC.Models.Dto;
+using MicroRabbit.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITransferService _transferService;
+
+        public HomeController(ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("home/transfer")]
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel model)
+        {
+            TransferDto transferDto = new TransferDto()
+            {
+                FromAccount = model.FromAccount,
+                ToAccount = model.ToAccount,
+                TransferAmount = model.TransferAmount
+            };
+
+            await _transferService.Transfer(transferDto);
+            return View("Index");
+        }
+    }
+}
